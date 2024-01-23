9c4f6575a3d14ce3f958f64f7a8a5ad1da692c44
Mayank Kumar Singh <mayank2220singh@gmail.com>
2024-01-23 12:52:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,4 +15,4 @@ HOP_LENGTH = 511
 STEP_SIZE = 32768 - HOP_LENGTH
 WINDOW_LENGTH = 1023
 POOL_SIZE = 6
-MESSAGE_POOL = np.load("dataset/message_pool.npy")
+MESSAGE_POOL = np.load("samples/message_pool.npy")
